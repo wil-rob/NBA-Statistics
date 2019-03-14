dec12c379268e53d31316e6587cc9ddda5bc0de6
William Robinson <45006045+wil-rob@users.noreply.github.com>
2019-03-13 23:17:04 -0400

    Add files via upload

---
+++ AnthonyDavis.py
@@ -0,0 +1,13 @@
+from ohmysportsfeedspy import MySportsFeeds
+
+Data_query = MySportsFeeds('2.0', verbose=True)
+Data_query.authenticate('7a623d32-510c-4ccf-8a7b-8a3af9', 'MYSPORTSFEEDS')
+
+Output = Data_query.msf_get_data(league='nba',
+                                 season='2018-2019-regular',
+                                 feed='seasonal_player_gamelogs',
+                                 player='anthony-davis',
+                                 format='json',
+                                 force='true')
+
+print(Output)
---
+++ Injuries.py
@@ -0,0 +1,12 @@
+from ohmysportsfeedspy import MySportsFeeds
+
+Data_query = MySportsFeeds('2.0', verbose=True)
+Data_query.authenticate('7a623d32-510c-4ccf-8a7b-8a3af9', 'MYSPORTSFEEDS')
+
+Output = Data_query.msf_get_data(league='nba',
+                                 season='2018-2019-regular',
+                                 feed='player_injuries',
+                                 format='json',
+                                 force='true')
+
+print(Output)
